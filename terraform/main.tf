76fe14e0d7904e70214f90fa42fd71961fb1bfda
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-09-09 17:32:12 +0100

    removed acr module

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,7 @@ module "vnet" {
 module "identity" {
   source = "./modules/identity"
   resource_group_name = module.resource_groups.resouce_group_name
-  acr_id = module.acr.acr_id
-}
-
-module "acr" {
-  source = "./modules/acr"
-  resource_group_name = module.resource_groups.resouce_group_name
+  acr_id = var.acr_id
 }
 
 module "resource_groups" {
@@ -21,7 +16,7 @@ module "resource_groups" {
 module "container_app" {
   source = "./modules/container-apps"
   container_apps_subnet_id = module.vnet.subnet_id
-  acr_login_server = module.acr.acr_login_server
+  acr_login_server = var.acr_login_server
   identity_id = module.identity.identity_id
   resource_group_name = module.resource_groups.resouce_group_name
 }
