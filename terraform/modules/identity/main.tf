5e0db443b8ce6ae53db213e2ff1b3d79292f3c0a
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-09-09 23:43:21 +0100

    modifications

--- terraform/modules/identity/main.tf
+++ terraform/modules/identity/main.tf
@@ -12,5 +12,5 @@ resource "azurerm_role_assignment" "acr_pull" {
   scope                = var.acr_id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_user_assigned_identity.main.principal_id
-  principal_type       = "ManagedIdentity"
+  principal_type       = "ServicePrincipal"
 }
