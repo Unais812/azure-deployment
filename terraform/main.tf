fdbae440015eb1581dd1bad7d60035c5119fe9dd
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-09-09 23:42:33 +0100

    changed vnet module path to networking

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-module "vnet" {
-  source = "./modules/vnet"
+module "networking" {
+  source = "./modules/networking"
   resource_group_name = module.resource_groups.resouce_group_name
 }
 
@@ -15,7 +15,7 @@ module "resource_groups" {
 
 module "container_app" {
   source = "./modules/container-apps"
-  container_apps_subnet_id = module.vnet.subnet_id
+  container_apps_subnet_id = module.networking.subnet_id
   acr_login_server = var.acr_login_server
   identity_id = module.identity.identity_id
   resource_group_name = module.resource_groups.resouce_group_name
