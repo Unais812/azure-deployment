50f86366e392b9db75738166b7de8f7756cd545c
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-09-09 17:30:22 +0100

    corrected name

--- terraform/modules/identity/main.tf
+++ terraform/modules/identity/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_user_assigned_identity" "main" {
   location            = var.location
-  name                = "identity-${resouce_group_name}"
+  name                = "identity-${var.resource_group_name}"
   resource_group_name = var.resource_group_name
   tags = {
     project = "2048"
@@ -12,5 +12,5 @@ resource "azurerm_role_assignment" "acr_pull" {
   scope                = var.acr_id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_user_assigned_identity.main.principal_id
-  principal_type       = "ServicePrincipal"
+  principal_type       = "ManagedIdentity"
 }
